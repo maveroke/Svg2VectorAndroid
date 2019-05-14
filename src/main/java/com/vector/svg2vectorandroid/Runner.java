2b849789729d0f8ebf1c0adbcff4868bfc05240a
Michael Whitehead <maveroke@gmail.com>
2019-05-15 06:15:04 +1000

    Manipulated to use output directory if one is provided

--- src/main/java/com/vector/svg2vectorandroid/Runner.java
+++ src/main/java/com/vector/svg2vectorandroid/Runner.java
@@ -1,5 +1,7 @@
 package com.vector.svg2vectorandroid;
 
+import java.nio.file.*;
+
 /**
  * Created by ravi on 19/12/17.
  */
@@ -11,9 +13,25 @@ public class Runner {
             System.out.println(" Provide source directory as first arguement for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
             return;
         }
+        String sourceDirectory = "";
+        String outputDirectory = "";
 
-        String sourceDirectory = args[0];
-        if(null != sourceDirectory && !sourceDirectory.isEmpty()){
+
+        if(args.length ==1)
+        {
+            sourceDirectory = args[0];
+        }
+
+        if(args.length > 2)
+        {
+            outputDirectory = args[1];
+        }
+
+        if(!sourceDirectory.isEmpty() && !outputDirectory.isEmpty()){
+            SvgFilesProcessor processor = new SvgFilesProcessor(sourceDirectory, outputDirectory);
+            processor.process();
+        }
+        else if(!sourceDirectory.isEmpty() ) {
             SvgFilesProcessor processor = new SvgFilesProcessor(sourceDirectory);
             processor.process();
         }
